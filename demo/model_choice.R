28a47b5d0af76cf92d2e9b0951406419a4975481
Sergio Venturini <sergio.venturini@unito.it>
2020-08-01 07:40:02 +0000

    version 0.4.0

--- demo/model_choice.R
+++ demo/model_choice.R
@@ -15,7 +15,7 @@ set.seed(seed)
 
 control <- list(burnin = burnin, nsim = nsim, z.prop = prm.prop[["z"]],
   alpha.prop = prm.prop[["alpha"]], random.start = TRUE, verbose = TRUE,
-  thin = 10, store.burnin = TRUE)
+  thin = 10, store.burnin = TRUE, procrustes = TRUE, relabel = FALSE)
 sim.ic <- dmbc_IC(data = simdiss, pmax = pmax, Gmax = Gmax, control = control,
   est = "mean")
 
@@ -28,6 +28,7 @@ new.ic <- update(sim.ic, pmax = pmax, Gmax = Gmax)
 summary(new.ic)
 
 # plot the results
+library(bayesplot)
 color_scheme_set("mix-yellow-blue")
 p <- plot(new.ic, size = c(4, 1.5))
 p + panel_bg(fill = "gray90", color = NA)
